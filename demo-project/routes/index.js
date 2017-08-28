d4826e50e0c7112bd4b8f6653cb9cc3976a5c358
humafarheen <huma.farheen94@gmail.com>
2017-08-28 22:26:53 +0530

    mongodb

--- demo-project/routes/index.js
+++ demo-project/routes/index.js
@@ -1,26 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Form Validation', errors: req.session.errors, success: req.session.success });
-  req.session.errors = null;
+  res.render('index');
 });
 
+router.get('/get-data', function(req,res,next) {
+  mongo.connect(url, function(err, db) {
+    var resultArray = [];
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null,err);
+      resultArray.push(doc);
+    }, function( {
+      db.close();
+      res.render('index', {items: resultArray});
+    }));
+  });
+});
+
+router.post('/insert', function(req,res,next) {
+  mongo.connect(url, function(err,db) {
+    var item = req.body.item;
+    assert.equal(null,err);
+    db.collection('user-data').insertOne(item, function(error, result) {
+      assert.equal(null,error);
+      console.log("Item inserted");
+      db.close();
+    });
+  });
+});
 router.post('/submit', function(request, response, next) {
-  var email = request.body.email;
-  var password = request.body.password;
-  var confirm_password = request.body.confirm_password;
-  request.check('email', 'Invalid email address').isEmail();
-  request.check('password', 'Password is invalid').isLength({ min: 4 });
-  request.check('confirm_password', 'Password should match with confirm password').equals(password);
-  var errors = request.validationErrors();
-  if(errors) {
-    request.session.errors = errors;
-    response.redirect('/');
-  } else {
-    request.session.success = true;
-  }
-  response.redirect('/');
+
 });
 module.exports = router;
